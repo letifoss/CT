3c2bb3a28391b5822da6e039f27cd573ba8f6177
letifoss <letifossemale@gmail.com>
2024-09-01 12:07:41 -0300

    ajustes imagenes

--- src/componentes/Footer.js
+++ src/componentes/Footer.js
@@ -7,10 +7,10 @@ function Footer(){
             <div class="d-flex align-items-center">
             <img src={logo} alt="" width="80px" height="80px" class="mx-4" />
             <div>
-                <a class="nav-link" aria-current="page" href="#home">Volver al inicio</a>
-                <a class="nav-link" href="#datos">Datos de interés</a>
-                <a class="nav-link" href="#form">Planifica tu viaje</a>
-                <a class="nav-link" href="#">Contacto</a>
+                <a class="nav-link mb-3" aria-current="page" href="#home"><b>Volver al inicio</b></a>
+                <a class="nav-link mb-2" href="#datos">Datos de interés</a>
+                <a class="nav-link mb-2" href="#form">Planifica tu viaje</a>
+                <a class="nav-link mb-2" href="#">Contacto</a>
             </div>
             </div>
             <div>
