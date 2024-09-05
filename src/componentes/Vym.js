f11aa5d702cbc9f2f13b90f0e3690a534527e4a5
letifoss <letifossemale@gmail.com>
2024-09-05 20:41:20 -0300

    ajustes

--- src/componentes/Vym.js
+++ src/componentes/Vym.js
@@ -10,7 +10,7 @@ function Vym (){
             <p className="p-3 w-md-75 mt-3 text-center "> Proponemos un escenario de fruto posible para la comunidad local a través de la planificación turística, de generar espacios de cuidados y de generar recursos para la conservación del medio.</p>
             <p className="p-3 w-md-75 mt-3 text-center ">Convertir al Cerro de Montevideo en un barrio turístico, pero sobre todo en un modelo de turismo sostenible que cumpla los requerimientos de la OMT y la UNESCO.</p>
         </div>
-        <div className="d-flex flex-column justify-content-start mt-5 boxinfo2 m-4 p-1" >
+        <div className="d-flex flex-column justify-content-start mt-5 boxinfo2 m-4" >
             <h4 className="mt-2 text-center">Misión</h4>
             <p className="w-md-75 mt-3 text-center">Desarrollar la actividad turística de manera responsable, asegurando así para las futuras generaciones la conservación del patrimonio natural y cultural tangible e intangible.</p>
             <p className="w-md-75 mt-3 text-center">Revalorización de espacios con potencial turístico para que puedan convertirse en un recurso turístico.</p>
