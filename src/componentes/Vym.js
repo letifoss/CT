235283756d6c2daaed3a99ae98bedb1a32976446
letifoss <letifossemale@gmail.com>
2024-09-05 20:31:57 -0300

    ajustes responsive

--- src/componentes/Vym.js
+++ src/componentes/Vym.js
@@ -10,11 +10,11 @@ function Vym (){
             <p className="p-3 w-md-75 mt-3 text-center "> Proponemos un escenario de fruto posible para la comunidad local a través de la planificación turística, de generar espacios de cuidados y de generar recursos para la conservación del medio.</p>
             <p className="p-3 w-md-75 mt-3 text-center ">Convertir al Cerro de Montevideo en un barrio turístico, pero sobre todo en un modelo de turismo sostenible que cumpla los requerimientos de la OMT y la UNESCO.</p>
         </div>
-        <div className="d-flex flex-column justify-content-start mt-5 boxinfo2 m-4" >
+        <div className="d-flex flex-column justify-content-start mt-5 boxinfo2 m-4 p-1" >
             <h4 className="mt-2 text-center">Misión</h4>
-            <p className="p-3 w-md-75 mt-3 text-center">Desarrollar la actividad turística de manera responsable, asegurando así para las futuras generaciones la conservación del patrimonio natural y cultural tangible e intangible.</p>
-            <p className="p-3 w-md-75 mt-3 text-center">Revalorización de espacios con potencial turístico para que puedan convertirse en un recurso turístico.</p>
-            <p className="p-3 w-md-75 mt-3 text-center">Trabajar en una nueva cultura de visitantes comprometidos con el medio ambiente y la comunidad local.</p>
+            <p className="w-md-75 mt-3 text-center">Desarrollar la actividad turística de manera responsable, asegurando así para las futuras generaciones la conservación del patrimonio natural y cultural tangible e intangible.</p>
+            <p className="w-md-75 mt-3 text-center">Revalorización de espacios con potencial turístico para que puedan convertirse en un recurso turístico.</p>
+            <p className="w-md-75 mt-3 text-center">Trabajar en una nueva cultura de visitantes comprometidos con el medio ambiente y la comunidad local.</p>
         </div>
         </div>
     </section>
