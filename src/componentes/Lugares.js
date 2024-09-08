954f2c4a82494e2e2fcacac23c655e566fba5e76
letifoss <letifossemale@gmail.com>
2024-09-08 13:23:55 -0300

    nueva seccion lugares

--- src/componentes/Lugares.js
+++ src/componentes/Lugares.js
@@ -3,41 +3,43 @@ import img1 from "../imagenes/9.jpeg"
 
 function Lugares(){
     return(
-        <div className="lugares w-100">
-        <h2 className="text-center mt-4">Lugares que podes conocer</h2>
+        <div className="d-flex justify-content-center">
+        <div className="lugares" data-aos="fade-up">
+        <h2 className="text-center mt-4 zindex text-white">Lugares que podes conocer</h2>
         <div className="d-flex flex-row justify-content-center flex-wrap">
-            <div className="d-flex flex-column align-items-center p-2">
+            <div className="d-flex flex-column align-items-center p-2 zindex">
                 <img src={img1} alt="" width={350} />
-                <p className="p-2">Nombre del lugar</p>
+                <p className="p-2 text-white">Nombre del lugar</p>
             </div>
-            <div className="d-flex flex-column align-items-center p-2">
+            <div className="d-flex flex-column align-items-center p-2 zindex">
                 <img src={img1} alt="" width={350} />
-                <p className="p-2">Nombre del lugar</p>
+                <p className="p-2 text-white">Nombre del lugar</p>
             </div>
-            <div className="d-flex flex-column align-items-center p-2">
+            <div className="d-flex flex-column align-items-center p-2 zindex">
                 <img src={img1} alt="" width={350} />
-                <p className="p-2">Nombre del lugar</p>
+                <p className="p-2 text-white">Nombre del lugar</p>
             </div>
-            <div className="d-flex flex-column align-items-center p-2">
+            <div className="d-flex flex-column align-items-center p-2 zindex">
                 <img src={img1} alt="" width={350} />
-                <p className="p-2">Nombre del lugar</p>
+                <p className="p-2 text-white">Nombre del lugar</p>
             </div>
-            <div className="d-flex flex-column align-items-center p-2">
+            <div className="d-flex flex-column align-items-center p-2 zindex">
                 <img src={img1} alt="" width={350} />
-                <p className="p-2">Nombre del lugar</p>
+                <p className="p-2 text-white">Nombre del lugar</p>
             </div>
-            <div className="d-flex flex-column align-items-center p-2">
+            <div className="d-flex flex-column align-items-center p-2 zindex">
                 <img src={img1} alt="" width={350} />
-                <p className="p-2">Nombre del lugar</p>
+                <p className="p-2 text-white">Nombre del lugar</p>
             </div>
-            <div className="d-flex flex-column align-items-center p-2">
+            <div className="d-flex flex-column align-items-center p-2 zindex">
                 <img src={img1} alt="" width={350} />
-                <p className="p-2">Nombre del lugar</p>
+                <p className="p-2 text-white">Nombre del lugar</p>
             </div>
-            <div className="d-flex flex-column align-items-center p-2">
+            <div className="d-flex flex-column align-items-center p-2 zindex">
                 <img src={img1} alt="" width={350} />
-                <p className="p-2">Nombre del lugar</p>
+                <p className="p-2 text-white">Nombre del lugar</p>
             </div>
+        </div>
         </div>
         </div>
     )
