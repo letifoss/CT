cdafa7066fe50c24a4e3885731f16517f7917e09
letifoss <letifossemale@gmail.com>
2024-09-22 19:07:07 -0300

    ajustes header

--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -10,7 +10,7 @@ function Header (){
         <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active">
-            <img src={header1} class="d-block w-100" alt="..."/>
+            <img src={header3} class="d-block w-100" alt="..."/>
             </div>
             <div class="carousel-item">
             <img src={header2}class="d-block w-100" alt="..."/>
