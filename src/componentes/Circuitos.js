0e7aae5c5455281a817f918f31b40172e8dd8a1c
letifoss <letifossemale@gmail.com>
2024-09-26 22:24:44 -0300

    ajustes circuitos

--- src/componentes/Circuitos.js
+++ src/componentes/Circuitos.js
@@ -15,6 +15,7 @@ function Circuitos(){
                 <h6 class="text-center w-100 zindexc mt-2 p-4">
                     Un circuito ideal para quienes viajan solos o en grupo y desean conocer y explorar lo más destacado del Cerro de Montevideo en poco tiempo.
                 </h6>
+                <button type="submit" class="p-1 price rounded"> <a href="https://wa.me/+59891626636" target="_blank"> Consultar precio</a></button>
             </div>
             <div class="d-flex flex-column align-items-center zindexc w-75 mt-5">
                 <img src={imagen3c} alt="imagen1c" width={280} />
@@ -22,6 +23,7 @@ function Circuitos(){
                 <h6 class="text-center w-100 zindexc mt-2 p-4">
                     Este Tour ganador del Sello Montevideo + verde, es un verdadero taller de naturaleza en el que recorremos distintos puntos del Oeste, empezando por el Cerro haciendo avistamiento de aves en el parque Débora Céspedes e interpretamos la relación de las industrias en las costas y la dinámica costera poniendo énfasis en la importancia del medio ambiente.
                 </h6>
+                <button type="submit" class="p-1 price rounded"> <a href="https://wa.me/+59891626636" target="_blank"> Consultar precio</a></button>
             </div>
             <div class="d-flex flex-column align-items-center zindexc w-75 mt-5">
                 <img src={imagen2c} alt="imagen1c" width={280} />
@@ -29,13 +31,15 @@ function Circuitos(){
                 <h6 class="text-center w-100 zindexc mt-2 p-4">
                     Un recorrido de 3 hs por exposiciones de arte a cielo abierto y cerradas, como la muestra de Eduardo Labraga en la Casa de la Pólvora y la de Gurvich en el FLorencio Sanchez. Te harán vibrar todos los sentidos y expresiones de un barrio a través del arte.
                 </h6>
+                <button type="submit" class="p-1 price rounded"> <a href="https://wa.me/+59891626636" target="_blank"> Consultar precio</a></button>
             </div>
             <div class="d-flex flex-column align-items-center zindexc w-75 mt-5">
                 <img src={imagen4c} alt="imagen1c" width={280} />
                 <h5 class="zindexc mt-3">El camino del inmigrante</h5>
                 <h6 class="text-center w-100 zindexc mt-2 p-4">
                     El Cerro de Montevideo fue creado en 1834 como barrio satélite para albergar la inmimgración que iba a ser muy numerosa y lo fue, por eso las calles llevan los nombres de todas las nacionalidades que provenían. Este tour recrea como fue la vida de estos inmigrantes en el Cerro, pasando por su trabajo en los frigoríficos, visitamos el ex Artigas, hoy PTI Cerro, clubes sociales como el Lituano, hasta las distintas formas de construir las casillas de chapa.
-                                    </h6>
+                 </h6>
+                 <button type="submit" class="p-1 price rounded"> <a href="https://wa.me/+59891626636" target="_blank"> Consultar precio</a></button>
             </div>
         </div>
     </div>
